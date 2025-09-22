49325a8f4998ff0a5b573714e492744d13f87b07
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-22 17:15:19 -0300

    chore: update openapi
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -3329,6 +3329,8 @@ export interface operations {
             query?: {
                 /** @description search params */
                 search?: string;
+                /** @description filter by method (email, account, sms, signal, telegram, whatsapp, xmpp) */
+                method?: string;
                 /** @description the number of page to return. Default: 1 */
                 page?: number;
                 /** @description Total of elements to return. Default: 25 */
@@ -4057,7 +4059,7 @@ export interface operations {
                      * @enum {string|null}
                      */
                     identifyMethod?: "account" | "email" | null;
-                    /** @description Method used to sign the document, i.e. emailToken, account, clickToSign */
+                    /** @description Method used to sign the document, i.e. emailToken, account, clickToSign, sms, signal, telegram, whatsapp, xmpp */
                     signMethod?: string | null;
                     /** @description Identify value, i.e. the signer email, account or phone number */
                     identify?: string | null;
@@ -4121,7 +4123,7 @@ export interface operations {
                      * @enum {string|null}
                      */
                     identifyMethod?: "account" | "email" | null;
-                    /** @description Method used to sign the document, i.e. emailToken, account, clickToSign */
+                    /** @description Method used to sign the document, i.e. emailToken, account, clickToSign, sms, signal, telegram, whatsapp, xmpp */
                     signMethod?: string | null;
                     /** @description Identify value, i.e. the signer email, account or phone number */
                     identify?: string | null;
